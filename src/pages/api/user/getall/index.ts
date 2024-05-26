3d07eb762e4abfe618b072add6ab2617cb41cd70
William <williamalonso@outlook.com>
2024-05-26 17:26:34 -0300

    :zap: test

--- src/pages/api/user/getall/index.ts
+++ src/pages/api/user/getall/index.ts
@@ -1,34 +1,14 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '@/models/userModel';
-import connectToDatabase from '@/config/database';
-import { NextApiRequest, NextApiResponse } from 'next';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { getUsers } from '@/controllers/userController';
 
-
-/**
- * @swagger
- * /api/user/getall:
- *   get:
- *     summary: Retrieve a list of users
- *     description: Retrieve a list of all users from the database.
- *     responses:
- *       200:
- *         description: A list of users
- *         content:
- *           application/json:
- *             schema:
- *               type: array
- *               items:
- *                 $ref: '#/components/schemas/User'
- *       500:
- *         description: Internal server error
- */
-export const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
-  await connectToDatabase();
-  try {
-    const users = await User.find({});
-    res.status(200).json(users);
-  } catch (e) {
-    res.status(500).json({ message: 'Erro no controller ao buscar todos os usuarios' })
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch(req.method) {
+    case 'GET':
+      await getUsers(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+      break;
   }
-};
+}
