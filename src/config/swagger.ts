7e2b78eed211b1bed6aa87a52af3b7c54b02a482
William <williamalonso@outlook.com>
2024-05-26 14:34:45 -0300

    :zap: creating swagger doc

--- src/config/swagger.ts
+++ src/config/swagger.ts
@@ -7,7 +7,32 @@ const options = {
     info: {
       title: 'Documentação da API - Vamos Dividir',
       version: '1.0.0',
-      description: 'Esta é a documentaçao da API - Vamos Dividir',
+      description: 'Esta é a documentação da API - Vamos Dividir',
+    },
+    components: {
+      schemas: {
+        User: {
+          type: 'object',
+          properties: {
+            email: {
+              type: 'string',
+              description: 'The email of the user',
+              example: 'user@example.com',
+            },
+            password: {
+              type: 'string',
+              description: 'The hashed password of the user',
+              example: 'hashedpassword123',
+            },
+            jwtToken: {
+              type: 'string',
+              description: 'The JWT token for the user',
+              example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...',
+            },
+          },
+          required: ['email', 'password'],
+        },
+      },
     },
   },
   apis: ['./src/controllers/*.ts', './src/models/*.ts'], // Ajuste para o caminho correto
--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,7 +4,24 @@ import User from '@/models/userModel';
 import connectToDatabase from '@/config/database';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-// get all users
+/**
+ * @swagger
+ * /api/user/getall:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     description: Retrieve a list of all users from the database.
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ *       500:
+ *         description: Internal server error
+ */
 export const getUsers = async (req: NextApiRequest, res: NextApiResponse) => {
   await connectToDatabase();
   try {
