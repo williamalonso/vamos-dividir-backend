0c70db2df5daec1fe0f73c78c9459054a317dca3
William <williamalonso@outlook.com>
2024-05-26 02:02:51 -0300

    :sparkles: creating bcrypt function

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -17,7 +17,7 @@ const userSchema: Schema = new Schema({
   }
 });
 
-// Use a função de pré-salvamento definida no middleware
+// Faz um hash na senha do usuário antes de ser salva no banco de dados sempre que um novo usuário for criado ou a senha de um usuário existente for atualizada
 userSchema.pre<userInterface>('save', hashPassword);
 
 const User = mongoose.models.User || mongoose.model<userInterface>('User', userSchema);
