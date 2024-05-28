adf04daeb60cbc41b4f9574920c41a2abbb9d40f
William <williamalonso@outlook.com>
2024-05-28 17:47:14 -0300

    :zap: test

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -4,18 +4,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const allowCors = (fn: any) => async (req: NextApiRequest, res: NextApiResponse) => {
 
-  // const allowedOrigins = ['http://localhost:3000'];
-
-  // const origin = req.headers.origin;
-  // console.log(origin)
-  // if (origin && allowedOrigins.includes(origin)) {
-  //   res.setHeader('Access-Control-Allow-Origin', origin);
-  // }
-
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', 'www.google.com');
-  // Alternativamente, você pode permitir a origem dos headers da solicitação
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.google.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
