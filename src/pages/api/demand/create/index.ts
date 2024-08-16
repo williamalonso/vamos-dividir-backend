b2dfb526a2591d4322de173fe40747c50697bc23
William <williamalonso@outlook.com>
2024-08-15 22:44:54 -0300

    :zap: test

--- src/pages/api/demand/create/index.ts
+++ src/pages/api/demand/create/index.ts
@@ -5,6 +5,12 @@ import { createDemand } from '@/controllers/demandController';
 import authMiddleware from '@/middleware/authMiddleware';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+
+  // Adiciona cabeçalhos CORS
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Permite qualquer origem, ajuste conforme necessário
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
   if (req.method === 'POST') {
     // Chama o controlador para criar a demanda
     await createDemand(req, res);
